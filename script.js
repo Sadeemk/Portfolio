66f06e2b269341459fe1da0e9c29daff13949ddd
Sadeem Khan <imsadeem98@gmail.com>
2020-05-03 19:55:35 -0700

    homepage setup

--- script.js
+++ script.js
@@ -22,26 +22,3 @@ const navSlide = () => {
 };
 
 navSlide();
-
-// $(document).ready(function() {
-// 	// .scroll() creates an event when the user scrolls
-// 	$(window).scroll(function() {
-// 		// .scrollTop() retrieves vertical position of the scroll bar for the first element in a set of matched elements
-// 		var scroll = $(window).scrollTop();
-
-// 		var objectSelect = $('#profile');
-
-// 		// .offset() retrieves current position of element relative to document
-// 		var objectPosition = objectSelect.offset().top;
-
-// 		if (scroll > objectPosition) {
-// 			$('#menu').fadeIn(500);
-// 		} else {
-// 			$('#menu').fadeOut(500);
-// 		}
-// 	});
-// });
-
-// const app = () => {
-// 	navSlide();
-// };
